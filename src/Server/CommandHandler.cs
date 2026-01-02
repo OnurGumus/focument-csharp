1fee92f0cf67e50a9a6b4e29b9b373a5fc94c6c8
Onur Gumus <onur@outlook.com.tr>
2026-01-02 16:33:49 +0100

    Remove handler

--- src/Server/CommandHandler.cs
+++ src/Server/CommandHandler.cs
@@ -5,22 +5,10 @@
 // It serves as the entry point for the command side of CQRS.
 // =============================================================================
 
-using System;
-using System.Threading.Tasks;
 using Model;
 using static FCQRS.CSharp;
-using CID = FCQRS.Model.Data.CID;
-using AggregateId = FCQRS.Model.Data.AggregateId;
 
 namespace Server;
-
-// Handler delegate type - returns Task for easy async/await usage
-public delegate Task<FCQRS.Common.Event<TEvent>> Handler<TCmd, TEvent>(
-    Func<TEvent, bool> filter,
-    CID correlationId,
-    AggregateId aggregateId,
-    TCmd command)
-    where TEvent : notnull;
 
 // -----------------------------------------------------------------------------
 // COMMAND HANDLER INTERFACE
